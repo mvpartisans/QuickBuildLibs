d432420b40bf1bf939d43c930495f8f85e0c3539
imranansari <imran.iansari@gmail.com>
2016-05-23 21:51:30 -0700

    init

--- BuildLib.groovy
+++ BuildLib.groovy
@@ -36,7 +36,7 @@ def initBuildModuleDefinition() {
     //return (Map) jsonResp;
     Map buildDef = [
             "lfs-a": "https://github.com/mvpartisans/pipeline-as-code-demo",
-            "lfs-b": "https://github.com/mvpartisans/pipeline-as-code-demo",
+            "lfs-b": "https://github.com/jglick/simple-maven-project-with-tests.git",
             "lfs-c": "https://github.com/kishorebhatia/pipeline-as-code-demo",
             "lfs-d": "https://github.com/kishorebhatia/pipeline-as-code-demo"
     ]
